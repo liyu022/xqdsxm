643c60aee320d8c2855edc9fe128f5b995efdecc
wangqiangvip <252673421@qq.com>
2020-11-28 21:49:45 +0800

    2

--- src/pages/leader/proportion.vue
+++ src/pages/leader/proportion.vue
@@ -1,45 +1,50 @@
 <template>
   <div class="pro_page">
     <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark"
-          style="width: 100%;height: 100%!important;overflow:auto;" row-class-name="row_class" border
-          :row-style="{fontFamily: '宋体', fontSize: '12px',height:'40px'}" @expand-change="expandChange" >
-          <el-table-column type="expand">
-          <template slot-scope="props">
-          <el-table border :data="props.row.children" v-loading="props.row.loading" style="width: 100%"
-            >
+      style="width: 100%;height: 100%!important;overflow:auto;" row-class-name="row_class" border
+      :row-style="{fontFamily: '宋体', fontSize: '12px',height:'40px'}"  @expand-change="expandChange">
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-table border :data="props.row.children" style="width: 100%">
             <el-table-column type="index" label="序号" width="60px" align="center">
-      </el-table-column>
-      <el-table-column prop="ROLANAME" label="角色名称">
-      </el-table-column>
-      <el-table-column   label="评价角色">
-          <template slot-scope="scope">
-              <span v-if="scope.row.TYPE=='A'">机关科级干部</span>
-              <span v-if="scope.row.TYPE=='D'">站队科级干部</span>
-          </template>
-      </el-table-column>
-      <el-table-column prop="CREATENAME" label="创建人">
-      </el-table-column>
-      <el-table-column prop="CREATETIME" label="更新时间">
-      </el-table-column>
-      <el-table-column prop="PROPORTION" label="权重">
-      </el-table-column>
+            </el-table-column>
+            <el-table-column prop="ROLANAME" label="角色名称">
+            </el-table-column>
+            <el-table-column label="评价角色">
+              <template slot-scope="scope">
+                <span v-if="scope.row.TYPE=='A'">机关科级干部</span>
+                <span v-if="scope.row.TYPE=='D'">站队科级干部</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="CREATENAME" label="创建人">
+            </el-table-column>
+            <el-table-column prop="CREATETIME" label="更新时间">
+            </el-table-column>
+            <el-table-column prop="PROPORTION" label="权重">
+            </el-table-column>
           </el-table>
         </template>
       </el-table-column>
-          <el-table-column align="center" class-name="column-caoz" label="操作">
+      
+      <el-table-column align="center" prop="NAME" label="考核计划名称" width="260"></el-table-column>
+      <el-table-column align="center"  label="考核类型" width="160">
+             <template slot-scope="scope">
+                <span v-if="scope.row.TYPE=='后备干部推荐'" style="color:#F56C6C">{{scope.row.TYPE}}</span>
+                <span v-else style="color:#67C23A">{{scope.row.TYPE}}</span>
+             </template>
+      </el-table-column>
+      <el-table-column align="center" prop="DATA" label="考核时间" width="150"></el-table-column>
+      <el-table-column align="center" prop="STATE" label="考核计划状态"></el-table-column>
+      <el-table-column align="center" prop="CREATETIME" label="创建时间"></el-table-column>
+      <el-table-column align="center" class-name="column-caoz" label="操作">
 
             <template slot-scope="scope">
-              <el-button type="primary" size="mini" @click="getProportionByPlanid(scope.row)">分配权重</el-button>
-              <!-- <span style="color:#00a2fd;cursor: pointer;text-align: center" @click="showBhFormDia(scope.row)">详情</span> -->
+              <el-button v-if="scope.row.TYPE=='后备干部推荐'" type="primary" size="mini" @click="getProportionByPlanid(scope.row)">分配权重</el-button>
+       
             </template>
           </el-table-column>
-          <el-table-column align="center" prop="NAME" label="考核计划名称" width="260"></el-table-column>
-          <el-table-column align="center" prop="TYPE" label="考核类型" width="160"></el-table-column>
-          <el-table-column align="center" prop="DATA" label="考核时间" width="150"></el-table-column>
-          <el-table-column align="center" prop="STATE" label="考核计划状态"></el-table-column>
-          <el-table-column align="center" prop="CREATETIME" label="创建时间"></el-table-column>
-        </el-table>
-  
+    </el-table>
+
     <div class="footer">
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage"
         layout="total, sizes, prev, pager, next, jumper" :total="total">
@@ -66,41 +71,44 @@
         <el-button type="primary" @click="handelSubmit">提 交</el-button>
       </span>
     </el-dialog>
-     <el-dialog title="权重设置" :visible.sync="subDetail" width="90%" :close-on-click-modal="false">
-            <div class="dialog_warp">
-                 <el-table :data="subtableData" ref="multipleTable" border style="width: 100%">
-      <el-table-column type="index" label="序号" width="60px" align="center">
-      </el-table-column>
-      <el-table-column prop="ROLANAME" label="角色名称">
-      </el-table-column>
-      <el-table-column   label="评价角色">
-          <template slot-scope="scope">
+    <el-dialog title="权重设置" :visible.sync="subDetail" width="90%" :close-on-click-modal="false">
+      <div class="dialog_warp">
+        <el-table :data="subtableData" ref="multipleTable" border style="width: 100%">
+          <el-table-column type="index" label="序号" width="60px" align="center">
+          </el-table-column>
+          <el-table-column prop="ROLANAME" label="角色名称">
+          </el-table-column>
+          <el-table-column label="评价角色">
+            <template slot-scope="scope">
               <span v-if="scope.row.TYPE=='A'">机关科级干部</span>
               <span v-if="scope.row.TYPE=='D'">站队科级干部</span>
-          </template>
-      </el-table-column>
-      <el-table-column prop="CREATENAME" label="创建人">
-      </el-table-column>
-      <el-table-column prop="CREATETIME" label="更新时间">
-      </el-table-column>
-      <el-table-column prop="PROPORTION" label="权重">
-          <template slot-scope="scope">
+            </template>
+          </el-table-column>
+          <el-table-column prop="CREATENAME" label="创建人">
+          </el-table-column>
+          <el-table-column prop="CREATETIME" label="更新时间">
+          </el-table-column>
+          <el-table-column prop="PROPORTION" label="权重">
+            <template slot-scope="scope">
               <el-input :min="0" :max="1" v-model="scope.row.PROPORTION" type="number"> </el-input>
-          </template>
-      </el-table-column>
-    </el-table>
-            </div>
-            <span slot="footer" class="dialog-footer">
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <span slot="footer" class="dialog-footer">
         <el-button @click="subDetail = false">取 消</el-button>
         <el-button type="primary" @click="handelSubmitPRO">提 交</el-button>
       </span>
-        </el-dialog>
+    </el-dialog>
   </div>
 </template>
 
 <script>
   import * as approveApi from '@/api/approve'
   import * as api from '@/api/leader'
+  import {
+    treeData
+  } from '../../utils'
   export default {
     data() {
       return {
@@ -108,13 +116,13 @@
         tableData: [{
 
         }],
-        subtableData:[],
-        subDetail:false,
+        subtableData: [],
+        subDetail: false,
         form: {
           PROPORTION: 0
         },
         searchForm: {},
-        currentId:'',
+        currentId: '',
         total: 0,
         currentPage: 1,
         pageSize: 10,
@@ -126,48 +134,53 @@
     methods: {
       expandChange(row, expandedRows) {
 
-   
-          // 通过$set属性可设置loading实现实时加载loading效果(经过测试,通过$set直接给父层数据声明子层数据时会出现报错,所以才在获取父层表格数据时声明子层表格数据)
-         let parmas={
-          planid:row.ID
+
+        // 通过$set属性可设置loading实现实时加载loading效果(经过测试,通过$set直接给父层数据声明子层数据时会出现报错,所以才在获取父层表格数据时声明子层表格数据)
+        let parmas = {
+          planid: row.ID
         }
-          // this.$set(row, 'loading', true);
-          approveApi.getProportionByPlanid(parmas).then((res) => {
-            if (res.data.code === 0) {
+        this.$set(row, 'loading', true);
 
-              const taskData = JSON.parse(JSON.stringify(res.data.data));
-              // 遍历父层表格数据
-              this.$set(row, 'children', taskData);
+        approveApi.getProportionByPlanid(parmas).then((res) => {
+          if (res.data.code === 0) {
 
-              this.tableData.forEach((temp, index) => {
-                // 找到当前展开的行，把获取到的数据赋值进去
-                if (temp.ID === row.ID) {
-                  this.tableData[index].children = taskData || [];
-                }
-              });
-            }
-            this.$set(row, 'loading', false);
-          }, (err) => {
-            this.$set(row, 'loading', false);
-          });
-        
+            const taskData = JSON.parse(JSON.stringify(res.data.data));
+            // 遍历父层表格数据
+            // this.$set(row, 'children', taskData);
+
+            this.tableData.forEach((temp, index) => {
+              // 找到当前展开的行，把获取到的数据赋值进去
+              if (temp.ID === row.ID) {
+
+                this.tableData[index].children = taskData || [];
+              }
+            });
+          }
+
+          this.$set(row, 'loading', false);
+
+
+        }, (err) => {
+          this.$set(row, 'loading', false);
+        });
+
       },
-      getProportionByPlanid(row){
-        let parmas={
-          planid:row.ID
+      getProportionByPlanid(row) {
+        let parmas = {
+          planid: row.ID
         }
-         approveApi.getProportionByPlanid(parmas).then(res => {
-           if(res.data.code === 0){
-             this.subtableData = res.data.data
-             this.currentId=row.ID
-             this.subDetail=true
-           }else{
-             this.$message({
-               type:'error',
-               message:'获取失败'
-             })
-            this.subDetail=false
-           }
+        approveApi.getProportionByPlanid(parmas).then(res => {
+          if (res.data.code === 0) {
+            this.subtableData = res.data.data
+            this.currentId = row.ID
+            this.subDetail = true
+          } else {
+            this.$message({
+              type: 'error',
+              message: '获取失败'
+            })
+            this.subDetail = false
+          }
         })
       },
       handleSizeChange(val) {
@@ -189,11 +202,12 @@
         };
         api.cadreplanList(param).then(res => {
           if (res.data.code === 0) {
-            this.tableData = res.data.data.list
-            this.tableData.forEach(item=>{
-                item.loading=false
-                item.children=[]
+
+            res.data.data.list.forEach(item => {
+              item.children = []; // 添加子表格数据
+              item.loading = false
             })
+            this.tableData = res.data.data.list
             this.total = res.data.data.total
           }
         })
@@ -212,7 +226,7 @@
           "type": this.form.TYPE
         }
         approveApi.sysroleproportionUpdate(params).then(res => {
-      
+
           if (res.data.code == 0) {
             this.form = {}
             this.showView = false
@@ -220,31 +234,31 @@
           }
         })
       },
-      handelSubmitPRO(){
+      handelSubmitPRO() {
         let userid = JSON.parse(localStorage.getItem('userid'))
-          for (let i = 0; i < this.subtableData.length; i++) {
-             this.subtableData[i].PLANID=this.currentId
-             this.subtableData[i].CREATEBY=userid
-          }
-          // let params={
-          //   result:JSON.parse(JSON.stringify(this.subtableData)) , 
-          //   planid: this.currentId
-          // }
+        for (let i = 0; i < this.subtableData.length; i++) {
+          this.subtableData[i].PLANID = this.currentId
+          this.subtableData[i].CREATEBY = userid
+        }
+        // let params={
+        //   result:JSON.parse(JSON.stringify(this.subtableData)) , 
+        //   planid: this.currentId
+        // }
 
-           approveApi.sysroleproportion(this.subtableData).then(res => {
-           if(res.data.code === 0){
-              this.$message({
-               type:'success',
-               message:'分配成功'
-             })
-             this.subDetail=false
-           }else{
-             this.$message({
-               type:'error',
-               message:'获取失败'
-             })
-            this.subDetail=true
-           }
+        approveApi.sysroleproportion(this.subtableData).then(res => {
+          if (res.data.code === 0) {
+            this.$message({
+              type: 'success',
+              message: '分配成功'
+            })
+            this.subDetail = false
+          } else {
+            this.$message({
+              type: 'error',
+              message: '获取失败'
+            })
+            this.subDetail = true
+          }
         })
       }
     }
